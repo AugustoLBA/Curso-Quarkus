fbcf5d31cfccee0ff0dc895b3f167534f1d5288b
AugustoLBA <127113107+AugustoLBA@users.noreply.github.com>
2024-10-02 17:03:37 -0300

    Adicionando a classe FollowerResource

---
+++ src/main/java/io/github/augustolba/quarkussocial/rest/FollowerResource.java
@@ -0,0 +1,24 @@
+package io.github.augustolba.quarkussocial.rest;
+
+import io.github.augustolba.quarkussocial.domain.repository.FollowerRepository;
+import io.github.augustolba.quarkussocial.domain.repository.UserRepository;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("users/{userId}/followers")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class FollowerResource {
+
+    private final FollowerRepository repository;
+    private final UserRepository userRepository;
+
+    @Inject
+    public FollowerResource(FollowerRepository repository, UserRepository userRepository) {
+        this.repository = repository;
+        this.userRepository = userRepository;
+    }
+}
