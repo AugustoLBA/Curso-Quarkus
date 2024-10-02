beff4b238c30c945d2a6bfb622d1fce3047a93bf
AugustoLBA <127113107+AugustoLBA@users.noreply.github.com>
2024-10-02 16:56:16 -0300

    Adiconando a entidade Follower

---
+++ src/main/java/io/github/augustolba/quarkussocial/domain/model/Follower.java
@@ -0,0 +1,22 @@
+package io.github.augustolba.quarkussocial.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "followers")
+@Data
+public class Follower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User follower;
+}
