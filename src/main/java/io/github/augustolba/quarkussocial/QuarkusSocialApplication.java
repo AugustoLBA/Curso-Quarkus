05d067aff502208358edc5a5ddba38725932c9a5
AugustoLBA <127113107+AugustoLBA@users.noreply.github.com>
2024-10-07 11:27:15 -0300

    Adicionando swagger na aplicacao

---
+++ src/main/java/io/github/augustolba/quarkussocial/QuarkusSocialApplication.java
@@ -0,0 +1,22 @@
+package io.github.augustolba.quarkussocial;
+
+import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Quarkus Social",
+                version = "1.0",
+                contact = @Contact(
+                        name = "José Augusto",
+                        url = "https://github.com/AugustoLBA/Curso-Quarkus"),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html")
+                )
+)
+public class QuarkusSocialApplication extends Application {
+}
